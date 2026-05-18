34bad4888ecfb2b6a2a9eec9426d9d793447b4bb
gege1970 <jerome.dugast@laposte.net>
2026-05-18 22:44:42 +0200

    Delete helialtitude-site/src directory

--- helialtitude-site/src/main.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './style.css';
-function App(){return <div className='hero'><div className='content'><h4>HÉLIALTITUDE SIMULATION</h4><h1>Prenez les commandes d'un hélicoptère en simulation</h1><p>Une expérience immersive accessible à tous.</p><button>Demander un devis</button></div></div>}
-ReactDOM.createRoot(document.getElementById('root')).render(<App/>);
